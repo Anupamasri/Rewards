a23cafac42953016f1c186a4d3960a84d808f30b
Avinash Theppala <avinashtheppala@Avinashs-MacBook-Pro.local>
2021-08-27 09:39:32 -0500

    initial commit for rewards POC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import data from "./dataSet.json";
+import Transaction from './components/Transaction';
 
-function App() {
+const App = () => {
+  const { customerProfiles } = data;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table id="customers">
+        <tr>
+          <th>Customer ID</th>
+          <th>All Transactions with Prices</th>
+          <th>Last three months transactions</th>
+          <th>Total Rewards</th>
+          <th>lastThreeMonthsRewards</th>
+        </tr>
+        {customerProfiles.map(customer => (<Transaction customer={customer} customerId={customer.customerId} />))}
+      </table>   
     </div>
   );
 }
